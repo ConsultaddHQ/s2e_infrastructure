9134abed4915b6f1de22af8df6d34a616a6adaf4
vijaypatidar <vramotiya+github@gmail.com>
2025-06-20 12:18:59 +0530

    Removed backend.tf as it is optional and need only if want to persist tf state to s3

--- bedrock_knowledge_base/backend.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  backend "s3" {
-       bucket         = "hpf-se2-insfra-tf-data-fjdkjflf"
-       key            = "terraform/state/terraform.tfstate"
-       region         = "us-east-1"
-     }
-}
- 
